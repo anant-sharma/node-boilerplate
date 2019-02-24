44c94e8620ceaf494dada7c75db0b65166f40b8d
Anant Sharma <anantatnet@gmail.com>
2019-02-24 16:25:44 +0530

    AddedJest

---
+++ src/modules/v1/clock/test.ts
@@ -0,0 +1,17 @@
+/**
+ * This file contains the test case required
+ * to test clock implementation
+ */
+import * as moment from 'moment';
+import { Clock } from './';
+
+test('It should create instance of Clock', () => {
+    expect(new Clock()).toBeInstanceOf(Clock);
+});
+
+test('It should return current time', () => {
+    const clock = new Clock();
+    expect(Number(clock.getTimestamp())).toBeLessThanOrEqual(
+        Number(moment().format('x'))
+    );
+});
